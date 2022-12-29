a2497d3b11e762811c8fff59c85762d9a2cc05cf
sumittricon <sumit.anshul@triconinfotech.com>
2022-12-29 11:39:27 +0530

    Added Button condition to show and Hide Image

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,7 @@ export class ProductListComponent{
     pageTitle : string = "Product List";
     imageWidth = 50;
     imageMargin = 2;
+    showImage : boolean = false;
     products : any[] = [
     {
         "productId": 8,
@@ -30,4 +31,8 @@ export class ProductListComponent{
         "imageUrl": "assets/images/xbox-controller.png"
       }
     ]
+
+    toggleImage() : void{
+      this.showImage = !this.showImage;
+    }
 }
