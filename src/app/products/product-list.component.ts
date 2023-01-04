22ef1494afd62b510ab6f7ea60d66568486c6a95
sumittricon <sumit.anshul@triconinfotech.com>
2023-01-04 11:32:22 +0530

    Created a Service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from './product';
     templateUrl : './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle : string = "Product List";
     imageWidth = 50;
     imageMargin = 2;
@@ -29,26 +29,7 @@ export class ProductListComponent{
    
    filteredProducts : IProduct[] = []; 
     products : IProduct[] = [
-    {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2021",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "assets/images/saw.png"
-      },
-      {
-        "productId": 10,
-        "productName": "Video Game Controller",
-        "productCode": "GMG-0042",
-        "releaseDate": "October 15, 2020",
-        "description": "Standard two-button video game controller",
-        "price": 35.95,
-        "starRating": 4.6,
-        "imageUrl": "assets/images/xbox-controller.png"
-      }
+  
     ]
     
 
@@ -61,4 +42,12 @@ export class ProductListComponent{
 
       return this.products.filter((product : IProduct) => product.productName.toLowerCase().includes(filterBy));
     }
+
+    ngOnInit(): void{
+      this.listFilter = 'cart'
+    }
+
+  onRatingClicked(message : string) : void{
+    this.pageTitle = 'Product List: ' + message;
+  }
 }
--- src/app/shared/start.component.ts
+++ src/app/shared/start.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component,EventEmitter,Input, Output, OnChanges } from "@angular/core";
 
 
 @Component({
@@ -10,11 +10,16 @@ import { Component, OnChanges } from "@angular/core";
 
 export class StarComponent implements OnChanges{
 
-    rating : number =4;
+    @Input() rating : number = 0;
     cropWidth : number = 75;
+    @Output() ratingClicked : EventEmitter<string> = new EventEmitter<string>();
  
     ngOnChanges() : void{
         this.cropWidth = this.rating * 75/5;
+    }
+
+    onclick() : void{
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked!`)
     }
 }
 
